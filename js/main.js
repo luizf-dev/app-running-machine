c43d4433bc7ae88d8883bb0c94dc9a648f249e05
LUIZ FERNANDO VIDAL <luiz.fvidal@senacsp.edu.br>
2022-10-05 22:24:18 -0300

    subindo o projeto

--- js/main.js
+++ js/main.js
@@ -1,53 +1,86 @@
-//=============ATIVAR MODAL DE SUCESSO NA GRAVAÇÃO DOS DADOS==================
+/*
+! =============Ativa o modal de sucesso=========== */
 const openModalSuccess = () => document.getElementById('modalSuccess').classList.add('active')
 
 const closeModalSuccess = () => {
     document.getElementById('modalSuccess').classList.remove('active');
 }
-//==================================================================
+/*
+! ================================================ */
 
 
-//=============== EVENTOS DE CLICK DOS BOTÕES SUCCESS
+/*
+! =============== Eventos de click ============== */
 document.getElementById('modalCloseSuccess')
    .addEventListener('click', closeModalSuccess)
 
 document.getElementById('cancelarModalSuccess')
     .addEventListener('click', closeModalSuccess)
-//==============================================
+/*
+!============================================== */
 
 
-const registrosUl = document.querySelector('#registros');
-const metaSemana = document.querySelector('#metaSemanal');
-const listarAcumulado = document.querySelector('#acumulado');
-const listarRestante = document.querySelector('#restante');
+/*
+! ================= Captura os IDs dos elementos HTML ======= */
+
+const duracaoUL = document.querySelector('#duracao');
+const metaSemanalUL = document.querySelector('#metaSemanal');
+const acumulado = document.querySelector('#acumulado');
+const restante = document.querySelector('#restante');
+const metaZerada = document.querySelector('#metaZerada');
+const saldoPositivo = document.querySelector('#saldoPositivo');
 const form = document.querySelector('#form');
-const inputDiaSemana = document.querySelector('#dia');
-const inputDuracao = document.querySelector('#duracao');
+const inputDia = document.querySelector('#dia');
+const inputDuracao = document.querySelector('#diaDuracao');
+const msg = document.querySelector('#msg');
+
+/*
+! =========META SEMANAL==============*/
+/*
+? TRAZ OS VALORES DO LOCAL STORAGE E TRANSFORMA COM O PARSE */
+const getMetaSemanal =  JSON.parse(localStorage.getItem('metaSemanal'));
 
 
-//===================================================================================*
-//Meta semanal armazenada de forma estática por enquanto                             *
-const testeMetaSemanal = [                                       
-    {id: 1, meta: 120}                                           
-]                                                                   
+/*
+? ATRIBUI UMA LET AO RESULTADO VINDO DO LOCAL STORAGE E VERIFICA SE O VALOR É NULO, SE FOR NULO, OU SEJA
+? SE O LOCAL STORAGE DESSE REGISTRO ESTIVER VAZIO, ATRIBUI UM OBJETO COM O VALOR ZERO */
+let metaSemanal = localStorage.getItem('metaSemanal') !== null ? getMetaSemanal : [{id: 0, metaDuracao: 0}];
 
-//adiciona meta semamal ao dom
-const addmetaSemanal = valor =>{
 
-    const h1 = document.createElement('h1');
-    h1.innerHTML = `
-        ${valor.meta}    
-    `
-    metaSemana.append(h1);
+/*
+? FAZ UM LOOP COM O FOREACH NOS REGISTROS DA META DA SEMANA NO LOCAL STORAGE */
+const initMetaSemanal = () => {
+    metaSemanal.forEach(addMetaDOM);
+
 }
 
-//quando a página for carregada faz o loop nos regitros e  adiciona no DOM
-const initMetaSemanal = () => {
-    testeMetaSemanal.forEach(addmetaSemanal);
+/*
+? REMOVE O VALOR DA META SEMANAL DO LOCAL STORAGE E  DA O REFRESH NA PÁGINA */
+const removeMeta = () => {
+    localStorage.removeItem('metaSemanal');
+    location.reload();
+}
+
+/*
+? ADICIONA O VALOR DA META SEMANAL NO DOM   */
+const addMetaDOM = valorMeta => {
+
+    const li  = document.createElement('li');
+
+    li.innerHTML = `<p class="metaSemanal">${valorMeta.metaDuracao}min</p>`
+
+    if(valorMeta.metaDuracao === 0){
+        li.innerHTML = `${valorMeta.metaDuracao}min`
+    }
+
+    metaSemanalUL.append(li);
 }
 
 initMetaSemanal();
-//=====================================================================================
+
+//<button class="btn"  onclick="removeMeta()">X</button>
+/* 
+! ==================================*/
 
 
 
@@ -62,39 +95,61 @@ const removeValores = ID => {
     init();
 }
 
-//Adiciona o valores ao DOM
-const addValoresDom = valores => {
+/*
+? FUNÇÃO PARA ADICIONAR A DURAÇÃO DO EXERCÍCIO EM MINUTOS NO DOM */
+const addDuracaoDom = valores => {
 
     const li = document.createElement('li');
+
     li.innerHTML = `
         ${valores.name}
         <span>${valores.duracao} minutos</span>
         <button class="delete-btn" onClick="removeValores(${valores.id})">x</button>         
     `
-    registrosUl.append(li);
+    duracaoUL.append(li);
 }
 
-const atualizaRegistros = () => {
-    const valoresDuracao = valores.map(valores => valores.duracao);
-    const totalMinutos = valoresDuracao.reduce((accumulator, valores ) => accumulator + valores, 0 );
-    const metaDuracao = testeMetaSemanal.map(valor => valor.meta);
-    const restante = metaDuracao - totalMinutos;
+/*
+? ATUALIZA OS VALORES DOS MINUTOS DIÁRIOS NO DOM, ASSIM COMO A META SEMANAL, O ACUMULADO DE 
+? EXERCÍCIOS E O QUE FALTA PARA ATINGIR A META */
+const updateValues = () => {
 
-    if(restante == 0){
-        openModalSuccess();
+    const duracaoValores = valores.map(valores => valores.duracao);
+    const metaValores = metaSemanal.map(valorMeta => valorMeta.metaDuracao);
+    const totalAcumulado = duracaoValores.reduce((acumulado, valores) => acumulado + valores, 0);
+    const minutosRestantes = metaValores  - totalAcumulado;    
+    const minutosSobrando = totalAcumulado - metaValores;
+/*
+    ? VERIFICA SE O USUÁRIO BATEU A META SEMANAL*/
+    if(metaValores == 0 && totalAcumulado == 0){
+       const h1 = document.createElement('h1');
+       h1.innerHTML = `Seja bem-vindo! `;
+       msg.append(h1);
+    }else if(totalAcumulado >= metaValores ){
+        alert("Parabéns! Você completou sua meta!");
     }
 
-    metaSemana.textContent = `${metaDuracao} min`;
-    listarAcumulado.textContent = `${totalMinutos} min`;
-    listarRestante.textContent = `${restante} min`;
+    acumulado.textContent = `${totalAcumulado} min`;    
+
+    /*    
+    ? VERIFICA SE O USUÁRIO JÁ POSSUI MINUTOS ALÉM DA SUA META */
+    if(minutosRestantes < 0){
+        restante.textContent = ``;
+         metaZerada.textContent = `Meta Alcançada!! `;
+         saldoPositivo.textContent = `${Math.abs(minutosSobrando)} min`; 
+    }else{
+        restante.textContent = `${Math.abs(minutosRestantes)} min`;
+        metaZerada.textContent = ``;
+        saldoPositivo.textContent = `0 min`; 
+    }
 
 }
 
 //quando a página for carregada faz o loop nos regitros e  adiciona no DOM
 const init = () => {
-    registrosUl.innerHTML = '';
-    valores.forEach(addValoresDom);
-    atualizaRegistros();
+    duracaoUL.innerHTML = '';
+    valores.forEach(addDuracaoDom);
+    updateValues();
 }
 
 init();
@@ -108,16 +163,15 @@ const atualizarLocalStorage = () => {
 //Gera um id com um número aleatório para o valor
 const gerarID = () => Math.round(Math.random() * 1000);
 
-
 //Adiciona os valores em um novo array
 const addToArray = (diaNome, duracaoNome) =>{
-    valores.push({id: gerarID(), name: diaNome, duracao: Number(duracaoNome)});
+    valores.push({id: gerarID(), name: diaNome, duracao:Number(duracaoNome)});
 
 }
 
 //Limpar os inputs após o cadastro dos valores
 cleanInputs = () => {
-     inputDiaSemana.value = '';
+     inputDia.value = '';
      inputDuracao.value = '';
 }
 
@@ -126,7 +180,7 @@ const formSubmit =  event => {
 
     event.preventDefault();
 
-    const diaNome = inputDiaSemana.value.trim();
+    const diaNome = inputDia.value.trim();
     const duracaoNome = inputDuracao.value.trim();    
     emptyInputs = diaNome === '' || duracaoNome === '';
 
@@ -143,3 +197,9 @@ const formSubmit =  event => {
 }
 
 form.addEventListener('submit', formSubmit);
+
+
+
+
+
+
---
+++ js/metaSemanal.js
@@ -0,0 +1,35 @@
+const metaSemanalUl = document.querySelector('#metaSemanal');
+const form = document.querySelector('#formMetaSemanal');
+const input = document.querySelector('#inputMetaSemanal');
+
+/*
+? Limpa o input após o cadastro da meta semanal*/
+cleanInput = () => {
+     input.value = '';
+}
+
+/*
+? Gerar um id aleatório */
+const generateID = () => Math.round(Math.random() * 1000);
+
+/*
+? Listener de eventos do formulário*/
+form.addEventListener('submit', evento =>{
+    evento.preventDefault();
+
+    const metaSemanalName = input.value.trim();
+
+    if(metaSemanalName === ''){
+        alert('Preencha corretamente o campo!');
+        return;
+    }
+
+    const metaSemanal = [{id: generateID(), metaDuracao: metaSemanalName}];
+    localStorage.setItem('metaSemanal', JSON.stringify(metaSemanal));
+    cleanInput();
+    history.back();
+})
+
+
+
+
